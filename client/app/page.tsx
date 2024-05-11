788e4f8ab6aabfb3017bcfaf77a70fb56bd905c7
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-05-11 12:28:30 +0530

    fixing the input size

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Link from "next/link";
+
 export default function Home() {
   const [shortUrl, setShortUrl] = useState<string>("");
   const [longUrl, setLongUrl] = useState<string>("");
@@ -42,7 +43,7 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-24">
+    <main className="flex flex-col items-center justify-center min-h-screen p-4 md:p-24">
       <div className="relative z-[-1] flex items-center justify-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <h1 className="text-5xl my-2 text-center">
           Generate a Short Url in no time
@@ -61,13 +62,13 @@ export default function Home() {
         </a>
       </h2>
       <form
-        className="w-full mt-2 flex items-center justify-center"
+        className="w-full mt-2 flex flex-col items-center justify-center sm:flex-row"
         onSubmit={handleSubmit}
       >
         <input
           type="search"
           id="username"
-          className="block font-bold w-1/2 p-4 ps-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
+          className="block font-bold w-full sm:w-1/2 p-4 ps-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
           placeholder="Enter URL here..."
           required
           style={{
@@ -78,7 +79,7 @@ export default function Home() {
         />
         <button
           type="submit"
-          className="ml-2 bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-white dark:bg-gray-500 dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+          className="mt-2 sm:mt-0 ml-0 sm:ml-2 bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-white dark:bg-gray-500 dark:hover:bg-gray-600 dark:focus:ring-gray-800"
           style={{ height: "3rem", backgroundColor: "#FFD700", color: "black" }}
         >
           Generate ShortUrl
